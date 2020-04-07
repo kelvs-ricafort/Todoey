36317c26d8b7daa12647d839e60493175cd80dd7
kelvs-ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2020-04-07 20:03:47 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Kelvin on 07/04/2020.
+//  Copyright © 2020 Genashtim Innovative Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
