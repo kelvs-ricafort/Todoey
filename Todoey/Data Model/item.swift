a84c63628d04c06893293b2ee3e5a744353e9457
kelvs-ricafort <60914571+kelvs-ricafort@users.noreply.github.com>
2020-04-10 21:00:17 +0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Kelvin on 10/04/2020.
+//  Copyright © 2020 Genashtim Innovative Learning. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
